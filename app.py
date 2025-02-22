abf89f1265741ed8ba715dbba8b5ceae19ec7629
Javier Cruz <cruzjavi092@gmail.com>
2025-02-21 19:37:59 -0600

    Commit de mostrar alumnos en formato JSO

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hola mundo"
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cud_alumnos_user:lGU6kNpcgKTpkwGS1qZ1m1ExRdmHaA5I@dpg-cuqeell2ng1s73eqols0-a.oregon-postgres.render.com/cud_alumnos'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/alumnos')
-def get_alumnos():
-    return "Retornando todos los alumnos"
+db = SQLAlchemy(app)
+
+# Definición del modelo de la tabla 'estudiantes'
+class Estudiante(db.Model):
+    __tablename__ = 'alumnos'
+    no_control = db.Column(db.String, primary_key=True)
+    nombre = db.Column(db.String, nullable=True)
+    ap_paterno = db.Column(db.String, nullable=True)
+    ap_materno = db.Column(db.String, nullable=True)
+    semestre = db.Column(db.Integer, nullable=True)
+
+# Endpoint para obtener todos los estudiantes
+@app.route('/estudiantes', methods=['GET'])
+def obtener_estudiantes():
+    estudiantes = Estudiante.query.all()
+    lista_estudiantes = []
+    for estudiante in estudiantes:
+        lista_estudiantes.append({
+            'no_control': estudiante.no_control,
+            'nombre': estudiante.nombre,
+            'ap_paterno': estudiante.ap_paterno,
+            'ap_materno': estudiante.ap_materno,
+            'semestre': estudiante.semestre
+        })
+    return jsonify(lista_estudiantes)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
